c921939b6c2216f23c558580d6135c586f7f7d2d
Francisco Solla <francisco.solla@gmail.com>
2024-05-20 14:40:56 -0300

    improve hero banner responsiveness

--- app/ui/Banner.tsx
+++ app/ui/Banner.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 import banner0 from "@/public/banner-0.jpg";
 
 export const Banner: React.FC = () => (
-  <div className="lg:w-auto max-w-full max-h-[80vh] aspect-[252/189] flex justify-center">
+  <div className="lg:w-auto max-w-full max-h-[80vh] aspect-[252/189] flex justify-center flex-2">
     <Image
       className="w-[2016px] lg:w-auto max-w-full max-h-[80vh] aspect-[252/189]"
       src={banner0}
