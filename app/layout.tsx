32db2400b7db9416f917d0186d189a13226a7b9e
Francisco Solla <francisco.solla@gmail.com>
2024-05-21 14:35:36 -0300

    add vercel analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import { Analytics } from "@vercel/analytics/react";
 
 export const metadata: Metadata = {
   title: "Dom Drone - Serviços de Filmagem e Inspeção Aérea Profissional",
@@ -144,6 +145,7 @@ export default function RootLayout({
           dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd) }}
         />
         {children}
+        <Analytics />
       </body>
     </html>
   );
